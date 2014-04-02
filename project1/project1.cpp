ec37459cbc8ee93622eb8ede7e7a392279ec76d8
gjeck <gsjeckell@gmail.com>
2014-04-01 21:43:16 -0700

    Initial commit. Project1 set-up

---
+++ project1/project1.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : project1.cpp
+// Author      : Gregory Jeckell, Andrew Leader
+// Version     :
+// Description : Selection
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "COMPARE.c"
+
+void doalg(int n, int k);
+
+int main(void) {
+	std::cout << "hello world" << std::endl;
+	return EXIT_SUCCESS;
+}
+
+void doalg(int n, int k)
+{
+
+}
