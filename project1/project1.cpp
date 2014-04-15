67f8b3ad0c805854d6dad424aa5ec19b3c142f10
gjeck <gsjeckell@gmail.com>
2014-04-14 23:37:12 -0700

    quickSort implement

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -7,65 +7,99 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 #include "COMPARE.c"
 using namespace std;
 
-void doalg(int n, int k);
-void quickSort(int arr[], int left, int right);
+int doalg(int n, int k);
+vector<int> quickSort(vector<int>& v);
+vector<int> concatenate(const vector<int>& less, int pivot, const vector<int>& greater);
+vector<int> quickSelect(vector<int>& list, int left, int right, int n);
+void partition(vector<int>& list, int left, int right, int pivotIndex);
 
 int main() {
-	doalg(15, 3);
+	int total = 0;
+	int max = 1000;
+	for (int i=0; i < max; ++i) {
+		int c = doalg(15, 3);
+		cout << "comparison = " << c << endl;
+		total += c;
+	}
+	cout << "average = " << (double)total / max << endl;
 	return 0;
 }
 
-void doalg(int n, int k)
+int doalg(int n, int k)
 {
 	COMPARE(0, n);
 
-	int* indexArray = new int[n];
+	vector<int> indexArray;
 	for (int i=0; i < n; ++i) {
-		indexArray[i] = i+1;
+		indexArray.push_back(i+1);
 	}
 
-	quickSort(indexArray, indexArray[0], indexArray[n]);
-
-	for (int i=0; i < n; ++i) {
-		cout << indexArray[i] << " ";
+	vector<int> result = quickSort(indexArray);
+	int* best = new int[k+1];
+	for (int i=1; i <= k; ++i) {
+		best[i] = result[i-1];
 	}
+
+	for (int i=0; i < result.size(); ++i) {
+		cout << result[i] << " ";
+	} cout << endl;
+
+	int comparisons = COMPARE(-1, k, best);
+	return comparisons;
 }
 
 
-void quickSort(int arr[], int left, int right)
+vector<int> quickSort(vector<int>& v)
 {
-      int i = left, j = right;
-      int tmp;
-      int pivot = arr[(left + right) / 2];
+	if (v.size() <= 1) { return v; }
+	int pivot = v.size() / 2;
+	vector<int> less;
+	vector<int> greater;
 
-      /* partition */
-      cout << "comparing i=" << i << " j=" << j << endl;
-      while (COMPARE(i, j) == 2) {
-            while (COMPARE(arr[i], pivot) == 2)
-                  i++;
-            while (COMPARE(arr[j], pivot) == 1)
-                  j--;
-            if (COMPARE(i, j) == 2) {
-                  tmp = arr[i];
-                  arr[i] = arr[j];
-                  arr[j] = tmp;
-                  i++;
-                  j--;
-            }
-      }
+	int tmp = v[pivot];
+	v[pivot] = v[0];
+	v[0] = tmp;
 
-      /* recursion */
-      if (COMPARE(left, j) == 2)
-            quickSort(arr, left, j);
-      if (COMPARE(i, right) == 2)
-            quickSort(arr, i, right);
+	for (int i=1; i < v.size(); ++i) {
+		if (COMPARE(v[i], v[0]) == 1) {
+			less.push_back(v[i]);
+		} else {
+			greater.push_back(v[i]);
+		}
+	}
+
+	return concatenate(quickSort(less), v[0], quickSort(greater));
+}
+
+vector<int> concatenate(const vector<int>& less, int pivot, const vector<int>& greater)
+{
+	vector<int> v;
+	for (int i=0; i < less.size(); ++i) {
+		v.push_back(less[i]);
+	}
+	v.push_back(pivot);
+	for (int i=0; i < greater.size(); ++i) {
+		v.push_back(greater[i]);
+	}
+	return v;
+}
+
+vector<int> quickSelect(vector<int>& list, int left, int right, int n)
+{
+
+}
+
+void partition(vector<int>& list, int left, int right, int pivotIndex)
+{
+
 }
 
 /*
-void quickSort(int arr[], int left, int right)
+void quickSort(int* arr, int left, int right)
 {
       int i = left, j = right;
       int tmp;
